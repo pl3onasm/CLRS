f4a8d553b99204015436da987a770ba2ce6a493b
David <pl3onasm@gmail.com>
2022-12-04 15:34:05 +0100

    :tada:

---
+++ algorithms/dynamic-programming/longest-palin-sub/lps-1.c
@@ -0,0 +1,47 @@
+/* file: lps-1.c
+   author: David De Potter
+   description: Longest Palindromic Subsequence
+   This is a naive recursive implementation. 
+   It is exponential in time.
+   If you only want to compute the length of the LPS,
+   you can rewrite the code without the need for a
+   string copy and a separate palindrome checker. 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isPalindrome (int start, int end, char *s) {
+  if (start >= end) return 1;
+  if (s[start] != s[end]) return 0;
+  return isPalindrome(start+1, end-1, s);
+}
+
+void computeLPS (char *s, char *seq, int n, int idx, int subLen, int *maxLen, char *maxSeq) {
+  if (idx >= n) {
+    if (subLen > *maxLen && isPalindrome(0, subLen-1, seq)) {
+      *maxLen = subLen;
+      strcpy (maxSeq, seq);
+      maxSeq[subLen] = '\0';
+    }
+    return;
+  }
+  if (subLen < n) {    // take char at index idx from s
+    seq[subLen] = s[idx];    
+    computeLPS(s, seq, n, idx+1, subLen+1, maxLen, maxSeq);
+  }
+  // skip current char from s
+  computeLPS(s, seq, n, idx+1, subLen, maxLen, maxSeq);
+}
+
+
+int main(int argc, char **argv){
+  char s[] = "kateibdochpwhetorbilars";
+  char seq[50] = {0}, maxSeq[50] = {0};
+  int maxLen = 0;
+  computeLPS(s, seq, strlen(s), 0, 0, &maxLen, maxSeq);
+  printf("Length of LPS is %d\n", maxLen);
+  printf("A possibly non-unique LPS is: %s\n", maxSeq);
+  return 0; 
+}
