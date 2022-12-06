b546977ba796215f3f12bd5812ad880b67007281
David <pl3onasm@gmail.com>
2022-12-06 10:16:41 +0100

    :tada:

--- algorithms/dynamic-programming/longest-palin-sub/lps-2.c
+++ algorithms/dynamic-programming/longest-palin-sub/lps-2.c
@@ -1,10 +1,7 @@
-/* file: lcs-4.c
+/* file: lps-2.c
 * author: David De Potter
-* description: longest common subsequence
+* description: longest palindromic subsequence
 * bottom-up dynamic programming implementation
-* Notice that we can now work with sequences that  
-* are much longer than in the naive recursive 
-* implementation, thanks to the O(mn) running time.
 */ 
 
 #include <stdlib.h>
@@ -41,8 +38,8 @@ char *reverse (char *s) {
 }
 
 int fillTable (int **table, const char *a, const char *rev, int la){
-  /* fills the table with the lengths of the lcs's of the prefixes 
-      of a and b and returns the length of the lcs of a and b */
+  /* fills the table with the lengths of the lps's of the prefixes of a  
+     and rev, and returns the length of the lps */
   for (int i=1; i<=la; ++i)     // fill table in row-major order
     for (int j=1; j<=la; ++j){
       if (a[i-1] == rev[j-1]) table[i][j] = table[i-1][j-1] + 1; 
@@ -51,17 +48,17 @@ int fillTable (int **table, const char *a, const char *rev, int la){
   return table[la][la];
 } 
 
-void reconstructLcs (int **table, char *a, int x, int y, char *lcs, int z){
-  /* fills the string lcs in reverse order (index z starts at the end) 
+void reconstructLps (int **table, char *a, int x, int y, char *lps, int z){
+  /* fills the string lps in reverse order (index z starts at the end) 
      with the longest common subsequence */
   if (x == 0 || y == 0) return;
   if (table[x][y] == table[x-1][y]) 
-    reconstructLcs(table, a, x-1, y, lcs, z);
+    reconstructLps(table, a, x-1, y, lps, z);
   else if (table[x][y] == table[x][y-1]) 
-    reconstructLcs(table, a, x, y-1, lcs, z);
+    reconstructLps(table, a, x, y-1, lps, z);
   else {
-    lcs[z] = a[x-1]; 
-    reconstructLcs(table, a, x-1, y-1, lcs, z-1);
+    lps[z] = a[x-1]; 
+    reconstructLps(table, a, x-1, y-1, lps, z-1);
   }
 } 
 
@@ -75,14 +72,14 @@ int main (int argc, char *argv[]) {
   char *rev = reverse(a);
   int la = strlen(a);
   int **table = createTable(la+1, la+1); 
-  int lcslen = fillTable(table, a, rev, la); 
-  char *lcs = safeCalloc(lcslen+1, sizeof(char));
-  reconstructLcs(table, a, la, la, lcs, lcslen-1);
-  lcs[lcslen] = '\0';   
+  int lpslen = fillTable(table, a, rev, la); 
+  char *lps = safeCalloc(lpslen+1, sizeof(char));
+  reconstructLcs(table, a, la, la, lps, lpslen-1);
+  lps[lpslen] = '\0';   
   printf("Given string:\n%s\n", a);
-  printf("The length of the LPS is %d.\n", lcslen);
-  printf("A possibly non-unique LPS is %s.\n", lcs);
-  free(lcs); free2Dmem(table, la+1);
+  printf("The length of the LPS is %d.\n", lpslen);
+  printf("A possibly non-unique LPS is %s.\n", lps);
+  free(lps); free(rev); free2Dmem(table, la+1);
   return 0;
 }
 
