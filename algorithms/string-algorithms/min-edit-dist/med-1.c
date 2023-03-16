3055cc1c206e8174c2794e5c99922c51494e6a2c
David <pl3onasm@gmail.com>
2023-03-16 02:19:09 +0100

    :tada:

--- algorithms/string-algorithms/min-edit-dist/med-1.c
+++ algorithms/string-algorithms/min-edit-dist/med-1.c
@@ -1,15 +1,14 @@
 /* file: med-1.c
 * author: David De Potter
 * description: minimum edit distance
+*   bottom-up dynamic programming (with path reconstruction)  
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
 
 void *safeCalloc (int n, int size) {
   /* allocates n elements of size size, initializing them to 0, and
@@ -25,22 +24,8 @@ void *safeCalloc (int n, int size) {
 int **newM(int m, int n) {
   /* allocates and initializes a m x n matrix */
   int **arr = safeCalloc(m, sizeof(int *));
-  for (int i = 0; i < n; ++i)
+  for (int i = 0; i < m; ++i)
     arr[i] = safeCalloc(n, sizeof(int));
-  for (int i = 0; i < m; ++i)
-    for (int j = 0; j < n; ++j)
-      arr[i][j] = INT_MAX; // initialize to infinity
-  return arr;
-}
-
-char **newPath(int m, int n) {
-  /* allocates and initializes a m x n matrix */
-  char **arr = safeCalloc(m, sizeof(char *));
-  for (int i = 0; i < n; ++i)
-    arr[i] = safeCalloc(n, sizeof(char));
-  for (int i = 0; i < m; ++i)
-    for (int j = 0; j < n; ++j)
-      arr[i][j] = ' '; // initialize to empty
   return arr;
 }
 
@@ -50,184 +35,171 @@ void freeM(int **m, int n) {
   free(m);
 }
 
-void printM(int **arr, int m, int n) {
-  /* prints a m x n matrix */
-  for (int i = 0; i < m; ++i) {
-    for (int j = 0; j < n; ++j){
-      printf("%d", arr[i][j]);
-      if (j < n - 1) printf(" ");
-    }
-    printf("\n");
-  }
-}
+void med(int **dp, char *s1, char *s2, int len1, int len2, int *costs) {
+  /* bottom-up approach to get the minimum edit distance between s1[0..i] and s2[0..j] */
+  dp[len1][len2] = INT_MAX;                  
 
-void readM(int **arr, int m, int n) {
-  /* reads a m x n matrix */
-  for (int i = 0; i < m; ++i)
-    for (int j = 0; j < n; ++j)
-      scanf("%d", &arr[i][j]);
-}
+  for (int i = 0; i <= len1; ++i)    // empty target string, so                   
+    dp[i][0] = costs[2] * i;         // delete all chars of source
 
-void minDist(int **dp, char **path, char *s1, char *s2, int len1, int len2, int costs[]) {
-  /* computes the minimum edit distance between s1 and s2 */
-  for (int i = 0; i <= len1; ++i) dp[i][0] = i * costs[1];  // empty target string
-  for (int j = 0; j <= len2; ++j) dp[0][j] = j * costs[0];  // empty source string
+  for (int j = 0; j <= len2; ++j)    // empty source string, so 
+    dp[0][j] = j * costs[1];         // insert all chars of target
 
-  for (int i = 1; i <= len1; ++i) {
-    for (int j = 1; j <= len2; ++j) {
-      int cost = INT_MAX;
-      if (s1[i - 1] == s2[j - 1]) {
-        cost = dp[i - 1][j - 1] + costs[3]; // copy
-        path[i][j] = 'c';
-        dp[i][j] = cost;
-      }
-      if (s1[i - 2] == s2[j - 1] && dp[i - 2][j - 2] + costs[4] < cost) {
-        cost = dp[i - 2][j - 2] + costs[4];  // twiddle
-        path[i][j] = 't';
-        dp[i][j] = cost;
-      }
-      if (s1[i] == s2[j - 1] && dp[i][j - 1] + costs[0] < cost) {
-        cost = dp[i][j - 1] + costs[0];  // insert
-        path[i][j] = 'i';
-        dp[i][j] = cost;
-      }
+  for (int i = 1; i <= len1; ++i){
+    for (int j = 1; j <= len2; ++j){
+      int cost = INT_MAX; 
+
       
-      if (s1[i - 1] == s2[j] && dp[i - 1][j] + costs[1] < cost) {
-        printf("i = %d, j = %d, s1[i-1] = %c, s2[j] = %c\n", i, j, s1[i-1], s2[j]);
-        cost = dp[i - 1][j] + costs[1];  // delete
-        path[i][j] = 'd';
-        dp[i][j] = cost;
+
+      if (s1[i-1] == s2[j-1])
+        cost = MIN(costs[0] + dp[i-1][j-1], cost);    // copy
+
+      if (i > 1 && j > 1 && s1[i-1] == s2[j-2] && s1[i-2] == s2[j-1])
+        cost = MIN(costs[4] + dp[i-2][j-2], cost);    // twiddle
+
+      cost = MIN(costs[1] + dp[i][j-1], cost);        // insert
+      cost = MIN(costs[2] + dp[i-1][j], cost);        // delete
+      cost = MIN(costs[3] + dp[i-1][j-1], cost);      // replace
+
+      if (j == len2 &&  i < len1)  
+        // update the cost of killing the remaining source chars
+        dp[len1][len2] = MIN(costs[5] + dp[i-1][len2], dp[len1][len2]);
+
+      dp[i][j] = (i == len1 && j == len2) ? MIN(cost, dp[i][j]) : cost;
+    }
+  }
+}
+
+void print(char *result, int idx, int len) {
+  /* prints the temporary result string */
+  for (int i = 0; i < idx; ++i) printf("%c", result[i]);
+  printf("-");
+  for (int i = idx; i < len; ++i) printf("%c", result[i]);
+  printf("\n");
+}
+
+void printPath(int **dp, char *s1, char *s2, int len1, int len2, 
+  int *costs, char *result, int *resLen, int *resIdx) {
+  /* recursively prints the path showing the transformations from s1 to s2 */
+  if (len1 == 0 && len2 == 0 && dp[len1][len2] == 0) return;
+
+  int cost = dp[len1][len2];
+ 
+  // kill
+  if (len1 == strlen(s1) && len2 == strlen(s2)) 
+    // check if a kill operation was performed
+    for (int i = len1-1; i >= 0; --i) {
+      if (dp[i][len2] + costs[5] == dp[len1][len2]){
+        printPath (dp, s1, s2, i, len2, costs, result, resLen, resIdx);
+        *resLen = *resLen - (len1 - i);
+        result[*resLen] = '\0';
+        printf("  - Kill %d:\t\t", len1 - i);
+        print(result, *resIdx, *resLen);
+        return; 
       }
-
-      // perform replace iff it is cheaper than insert and delete
-      if (costs[2] < costs[0] + costs[1]) {
-        cost = dp[i - 1][j - 1] + costs[2];  // replace
-        path[i][j] = 'r';
-        dp[i][j] = cost;
-      } else { // perform insert and delete
-        path[i-1][j] = 'd';
-        path[i][j] = 'i';
-        dp[i][j] = dp[i-1][j-1] + costs[0] + costs[1];
-      }
-      
-      //if (j == len2 && i < len1 && cost + costs[5] < dp[len1][len2]) {
-        //dp[len1][len2] = cost + costs[5];  // kill
-        //path[len1][len2] = 'k';
-      //}
-      //if (i == len1 && j == len2 && cost > dp[i][j]) {
-        //path[i][j] = 'k';
-      //} else {
-        //dp[i][j] = cost;
-      //}
     }
+
+  // copy
+  if (len1 > 0 && len2 > 0 && s1[len1-1] == s2[len2-1] 
+      && cost == costs[0] + dp[len1-1][len2-1]){
+    printPath(dp, s1, s2, len1-1, len2-1, costs, result, resLen, resIdx);
+    (*resIdx)++; 
+    printf("  - Copy %c:\t\t", s1[len1-1]);
+    print(result, *resIdx, *resLen);
+
+  // twiddle
+  } else if (len1 > 1 && len2 > 1 && s1[len1-1] == s2[len2-2] 
+      && s1[len1-2] == s2[len2-1] && cost == costs[4] + dp[len1-2][len2-2]){
+    printPath(dp, s1, s2, len1-2, len2-2, costs, result, resLen, resIdx);
+    result[(*resIdx)++] = s1[len1-1];
+    result[(*resIdx)++] = s1[len1-2];
+    printf("  - Twiddle %c and %c:\t", s1[len1-1], s1[len1-2]);
+    print (result, *resIdx, *resLen);
+
+  // insertion
+  } else if (len2 > 0 && cost == costs[1] + dp[len1][len2-1]){
+    printPath(dp, s1, s2, len1, len2-1, costs, result, resLen, resIdx);
+    (*resLen)++; 
+    for (int i = *resLen; i >= *resIdx; --i) result[i+1] = result[i];
+    result[(*resIdx)++] = s2[len2-1];
+    result[*resLen] = '\0';
+    printf("  - Insert %c:\t\t", s2[len2-1]);
+    print(result, *resIdx, *resLen);
+
+  // deletion
+  } else if (len1 > 0 && cost == costs[2] + dp[len1-1][len2]){
+    printPath(dp, s1, s2, len1-1, len2, costs, result, resLen, resIdx);
+    for (int i = *resIdx; i < *resLen; ++i) result[i] = result[i+1];
+    (*resLen)--;
+    printf("  - Delete %c:\t\t", s1[len1-1]);
+    print(result, *resIdx, *resLen);
+
+  // replacement
+  } else if (len1 > 0 && len2 > 0 && cost == costs[3] + dp[len1-1][len2-1]){
+    printPath(dp, s1, s2, len1-1, len2-1, costs, result, resLen, resIdx);
+    result[*resIdx] = s2[*resIdx]; 
+    (*resIdx)++;
+    printf("  - Replace %c with %c:\t", s1[len1-1], s2[len2-1]); 
+    print(result, *resIdx, *resLen);
   }
 }
 
-void reconstruct(int **dp, char** path, char *s1, char *s2, int len1, int len2, char *result, int *resPos, int resLen, int *step) {
-  /* reconstructs the minimum edit distance between s1 and s2 and shows how s1 was transformed into s2 using
-     result */
-  if (len1 == 0 && len2 == 0) return;
-  if (path[len1][len2] == 'c') {
-    reconstruct(dp, path, s1, s2, len1 - 1, len2 - 1, result, resPos, resLen, step);
-
-    result[(*resPos)++] = s1[len1 - 1];
-    printf("%d. Copy %c at index %d: %s\n", (*step)++, s1[len1 - 1], *resPos - 1, result); // copy
-    
-  } else if (path[len1][len2] == 't') {
-    reconstruct(dp, path, s1, s2, len1 - 2, len2 - 2, result, resPos, resLen, step);
-
-    result[(*resPos)++] = s1[len1 - 2];
-    result[(*resPos)++] = s1[len1 - 1];
-    printf("twiddle %c%c: %s\n", s1[len1 - 2], s1[len1 - 1], result); // twiddle
-    
-  } else if (path[len1][len2] == 'r') {
-    reconstruct(dp, path, s1, s2, len1 - 1, len2 - 1, result, resPos, resLen, step);
-    printf("%c %d\n", s1[len1 - 1], len1 - 1);
-    printf("%c %d\n", s2[len2 - 1], len2 - 1);
-    result[(*resPos)++] = s2[len2 - 1];
-    printf("%d. Replace %c with %c at index %d: %s\n", (*step)++, s1[len1 - 1], s2[len2 - 1], *resPos - 1, result); // replace
-
-  } else if (path[len1][len2] == 'd') {
-    reconstruct(dp, path, s1, s2, len1 - 1, len2, result, resPos, resLen, step);
-    // delete s1[len1 - 1]
-    for (int i = *resPos; i < resLen - 1; ++i) {
-      result[i] = result[i + 1];
-    }
-    result[resLen - 1] = '\0';
-    printf("%d. Delete %c at index %d: %s\n", (*step)++, s1[len1 - 1], *resPos, result); // delete
-
-  } else if (path[len1][len2] == 'i') {
-    reconstruct(dp, path, s1, s2, len1, len2 - 1, result, resPos, resLen, step);
-    for (int i = resLen - 1; i > *resPos; --i) {
-      result[i] = result[i - 1];
-    }
-    result[(*resPos)++] = s2[len2 - 1];
-    printf("%d. Insert %c at index %d: %s\n", (*step)++, s2[len2 - 1], *resPos - 1, result); // insert
-    
-  } else if (path[len1][len2] == 'k') {
-    reconstruct(dp, path, s1, s2, len1, len2, result, resPos, resLen, step);
-
-    for (int i = *resPos; i < resLen - 1; ++i) {
-      result[i] = result[i + 1];
-    }
-    result[resLen - 1] = '\0';
-
-
-    printf("%d. Kill: %s\n", (*step)++, result); // kill
-
-
-  }
+void printResult(int **dp, char *s1, char *s2, int len1, int len2, int *costs) {
+  /* prints the minimum edit distance and the path */
+  printf("Task: source -> target\n      ");
+  printf("'%s' -> '%s'\n\n", s1, s2);
+  printf("Costs: copy = %d, insert = %d, delete = %d,\n       replace = %d, twiddle = %d"
+    ", kill = %d\n\n", costs[0], costs[1], costs[2], costs[3], costs[4], costs[5]); 
+  printf("Edit sequence:\n  - Source:\t\t'%s'\n", s1);
+  int resIdx = 0, resLen = len1;
+  char *result = (char *) malloc(sizeof(char) * (len1 + len2 + 2));
+  strcpy(result, s1); 
+  printPath(dp, s1, s2, len1, len2, costs, result, &resLen, &resIdx);
+  printf("  - Result:\t\t'%s'\n\n", s2);
+  printf("Minimum edit distance: %d\n", dp[len1][len2]);
+  free(result);
 }
 
-void printC(char **arr, int m, int n) {
-  /* prints a m x n matrix */
-  for (int i = 0; i < m; ++i) {
-    for (int j = 0; j < n; ++j){
-      printf("%c", arr[i][j]);
-      if (j < n - 1) printf(" ");
+char *readString(){
+  char n; int index = 0, cap = 128;
+  char *arr = calloc(cap,sizeof(char)); 
+  while (scanf("%c", &n) && n != '\n' && n != EOF){
+    index++;
+    if (index % cap == 0) {
+      arr = realloc(arr, 2*index*sizeof(char));
+      cap *= 2;
     }
-    printf("\n");
+    arr[index-1] = n;
   }
+  arr[index] = '\0';
+  return arr; 
 }
 
-void freePath (char **path, int m) {
-  /* frees a m x n matrix */
-  for (int i = 0; i < m; ++i) {
-    free(path[i]);
-  }
-  free(path);
+int *readCosts() {
+  /* reads the costs for the operations from stdin */
+  int *costs = (int *) malloc(sizeof(int) * 6);
+  for (int i = 0; i < 6; ++i) scanf("%d ", &costs[i]);
+  return costs;
 }
 
 int main (int argc, char *argv[]) {
-  char *s1 = "algorithm";    // source string
-  char *s2 = "altruistic";   // target string
+  char *s1 = readString();    // source string
+  char *s2 = readString();    // target string
+  
+  // operation costs for copy, insert, 
+  // delete, replace, twiddle, kill
+  int *costs = readCosts();   
+  
   int len1 = strlen(s1);
   int len2 = strlen(s2);
-
   int **dp1 = newM(len1+1, len2+1);
-  char **path = newPath(len1+1, len2+1);
 
-  int costs[] = {0 ,5, 6, 6, 10, 9};  // costs of insertion, deletion, replacement, copy, twiddle, kill
+  med(dp1, s1, s2, len1, len2, costs);
 
-  minDist(dp1, path, s1, s2, len1, len2, costs);
+  printResult(dp1, s1, s2, len1, len2, costs);
 
-  printM(dp1, len1+1, len2+1);
-
-  int editDist = dp1[len1][len2];
-  printf("Minimum edit distance: %d\n", editDist);
-
-  printC(path, len1+1, len2+1);
-  int resLen = MAX(len1, len2) + 1;
-  printf("resLen: %d\n", resLen);
-  char *result = (char *)malloc(sizeof(char) * (resLen));
-  // make copy of s1 and put it in result
-  strcpy(result, s1);
-  printf("Source string: %s\n", s1);
-  int resPos = 0, step = 1;
-  reconstruct(dp1, path, s1, s2, len1, len2, result, &resPos, resLen, &step);
-  
   freeM(dp1, len1+1);
-  freePath(path, len1+1);
-  free(result);
+  free(s1); free(s2);
+  free(costs);
   return 0;
 }
